aeaf35f8fc116f2e89f121c68b852a38b025897b
Skenzi <fitronavm@gmail.com>
2021-09-20 08:05:29 +0500

    remove errorsSlice

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './features/channelsSlice.js';
 import messagesReducer from './features/messagesSlice.js';
-import errorsReducer from './features/errorsSlice.js';
 
 const store = configureStore({
   reducer: {
     channelsData: channelsReducer,
     messagesData: messagesReducer,
-    errors: errorsReducer,
   },
 });
 
